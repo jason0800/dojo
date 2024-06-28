8c07b2944d41b4762a9e47f9a37a5498ddddbef3
Jason Wang <jasonwang332@gmail.com>
2024-06-28 15:30:13 +1000

    Added rubocop and fixed quoting
    With:
      bundle exec rubocop
      bundle exec rubocop --only Style/StringLiterals -a
    
    -a to autocorrect rubocop

--- 2024-06-28_Fizz_Buzz_rb/spec/fizz_buzz_spec.rb
+++ 2024-06-28_Fizz_Buzz_rb/spec/fizz_buzz_spec.rb
@@ -1,31 +1,31 @@
-require "fizz_buzz"
+require 'fizz_buzz'
 
 RSpec.describe FizzBuzz do
-  it "returns 1 for input 1" do
+  it 'returns 1 for input 1' do
     expect(FizzBuzz.new.output(1)).to eq 1
   end
 
-  it "returns 2 for input 2" do
+  it 'returns 2 for input 2' do
     expect(FizzBuzz.new.output(2)).to eq 2
   end
 
-  it "returns fizz for input 3" do
+  it 'returns fizz for input 3' do
     expect(FizzBuzz.new.output(3)).to eq 'fizz'
   end
 
-  it "returns buzz for input 5" do
+  it 'returns buzz for input 5' do
     expect(FizzBuzz.new.output(5)).to eq 'buzz'
   end
 
-  it "returns fizz for input 6" do
+  it 'returns fizz for input 6' do
     expect(FizzBuzz.new.output(6)).to eq 'fizz'
   end
 
-  it "returns fizz buzz for input 15" do
+  it 'returns fizz buzz for input 15' do
     expect(FizzBuzz.new.output(15)).to eq 'fizz buzz'
   end
 
-  it "returns fizz buzz numbers 1 to 100" do
+  it 'returns fizz buzz numbers 1 to 100' do
     expect(
       (1..100).map do |index|
         FizzBuzz.new.output(index)
@@ -33,104 +33,104 @@ RSpec.describe FizzBuzz do
     ).to eq [
       1,
       2,
-      "fizz",
+      'fizz',
       4,
-      "buzz",
-      "fizz",
+      'buzz',
+      'fizz',
       7,
       8,
-      "fizz",
-      "buzz",
+      'fizz',
+      'buzz',
       11,
-      "fizz",
+      'fizz',
       13,
       14,
-      "fizz buzz",
+      'fizz buzz',
       16,
       17,
-      "fizz",
+      'fizz',
       19,
-      "buzz",
-      "fizz",
+      'buzz',
+      'fizz',
       22,
       23,
-      "fizz",
-      "buzz",
+      'fizz',
+      'buzz',
       26,
-      "fizz",
+      'fizz',
       28,
       29,
-      "fizz buzz",
+      'fizz buzz',
       31,
       32,
-      "fizz",
+      'fizz',
       34,
-      "buzz",
-      "fizz",
+      'buzz',
+      'fizz',
       37,
       38,
-      "fizz",
-      "buzz",
+      'fizz',
+      'buzz',
       41,
-      "fizz",
+      'fizz',
       43,
       44,
-      "fizz buzz",
+      'fizz buzz',
       46,
       47,
-      "fizz",
+      'fizz',
       49,
-      "buzz",
-      "fizz",
+      'buzz',
+      'fizz',
       52,
       53,
-      "fizz",
-      "buzz",
+      'fizz',
+      'buzz',
       56,
-      "fizz",
+      'fizz',
       58,
       59,
-      "fizz buzz",
+      'fizz buzz',
       61,
       62,
-      "fizz",
+      'fizz',
       64,
-      "buzz",
-      "fizz",
+      'buzz',
+      'fizz',
       67,
       68,
-      "fizz",
-      "buzz",
+      'fizz',
+      'buzz',
       71,
-      "fizz",
+      'fizz',
       73,
       74,
-      "fizz buzz",
+      'fizz buzz',
       76,
       77,
-      "fizz",
+      'fizz',
       79,
-      "buzz",
-      "fizz",
+      'buzz',
+      'fizz',
       82,
       83,
-      "fizz",
-      "buzz",
+      'fizz',
+      'buzz',
       86,
-      "fizz",
+      'fizz',
       88,
       89,
-      "fizz buzz",
+      'fizz buzz',
       91,
       92,
-      "fizz",
+      'fizz',
       94,
-      "buzz",
-      "fizz",
+      'buzz',
+      'fizz',
       97,
       98,
-      "fizz",
-      "buzz"
+      'fizz',
+      'buzz'
     ]
   end
 end
