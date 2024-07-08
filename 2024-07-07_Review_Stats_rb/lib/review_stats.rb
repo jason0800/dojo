b97b2e89d9407a9ffea7f9421c919e4145168108
Jason Wang <jasonwang332@gmail.com>
2024-07-08 16:02:07 +1000

    wrote tests for median and count of 3 numbers

--- 2024-07-07_Review_Stats_rb/lib/review_stats.rb
+++ 2024-07-07_Review_Stats_rb/lib/review_stats.rb
@@ -2,20 +2,27 @@ require 'csv'
 
 class ReviewStats
   def self.process(csv_string)
-    CSV.parse(csv_string, headers: true).map{|elem| elem["rating"].to_i }
+    CSV.parse(csv_string, headers: true).map { |elem| elem["rating"].to_i }
   end
 
   def self.average(array)
-    if array.length == 0
-      return 0
-    end
+    return 0 if array.empty?
 
     (array.sum / array.length.to_f).round(2)
   end
 
+  def self.median(array)
+    return 0 if array.empty?
+  end
+
+  def self.count(array)
+    return 0 if array.empty?
+
+    array.length
+  end
+
   def self.output(values)
     "Average: #{values[:average]}\n"
-    # "Average: #{values[:average]}\n" +
     # "Count: #{values[:count]}\n" +
     # "Spread: #{values[:spread]}\n"
   end
--- 2024-07-07_Review_Stats_rb/spec/review_stats_spec.rb
+++ 2024-07-07_Review_Stats_rb/spec/review_stats_spec.rb
@@ -9,35 +9,58 @@ RSpec.describe ReviewStats do
     end
   end
 
-  context "when CSV has 2 ratings" do
+  context "when CSV has 3 ratings" do
     let(:csv) do
       <<~EO_CSV
         rating
         1
         2
+        3
       EO_CSV
     end
 
-    it "returns an array of 2 ratings" do
-      expect(ReviewStats.process(csv)).to eq([1,2])
+    it "returns an array of 3 ratings" do
+      expect(ReviewStats.process(csv)).to eq([1, 2, 3])
     end
   end
 
+  # Empty array
   context "when we have an empty array" do
     it "returns 0 for average" do
       input = []
       expect(ReviewStats.average(input)).to eq 0
     end
+
+    it "returns 0 for median" do
+      input = []
+      expect(ReviewStats.median(input)).to eq 0
+    end
+
+    it "returns 0 for count" do
+      input = []
+      expect(ReviewStats.count(input)).to eq 0
+    end
   end
 
+  # Array Calcs
   context "when we have an array with 3 numbers" do
     it "returns average of 3 numbers to 2 decimal places" do
       input = [1, 2, 2]
       expect(ReviewStats.average(input)).to eq 1.67
     end
+
+    it "returns median of 3 numbers" do
+      input = [1, 2, 2]
+      expect(ReviewStats.median(input)).to eq 2
+    end
+
+    it "returns count of array" do
+      input = [1, 2, 2]
+      expect(ReviewStats.count(input)).to eq 3
+    end
   end
 
-  # DISPLAY ONLY
+  # Display Only
   it "pretty displays our stats" do
     expect(ReviewStats.output(average: 0)).to eq <<~EO_DISPLAY
       Average: 0
